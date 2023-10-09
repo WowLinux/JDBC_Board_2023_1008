1f4373b26648b9a5edfdc2f871831830a17b783e
Wowlinux <jhjeong@wowlinux.co.kr>
2023-10-09 11:37:50 +0900

    게시물 작성시 DB에 저장

--- db.sql
+++ db.sql
@@ -1,3 +1,23 @@
 DROP DATABASE IF EXISTS text_board;
 CREATE DATABASE text_board;
 use text_board;
+
+DROP DATABASE IF EXISTS text_board;
+CREATE DATABASE text_board;
+use text_board;
+
+CREATE TABLE article (
+   id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   regDate DATETIME NOT NULL,
+   updateDate DATETIME NOT NULL,
+   title CHAR(100) NOT NULL,
+   `body` TEXT NOT NULL
+   );
+
+   SELECT * FROM article;
+
+   INSERT INTO article
+   SET regDate =NOW(),
+   updateDate = NOW(),
+   title = CONCAT("제목", RAND()),
+   `body` = CONCAT("내용", RAND());
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -23,9 +27,46 @@ public class App {
                 String body = sc.nextLine();
                 int id= ++articleLastId;
 
+                Connection conn = null;
+                PreparedStatement pstmt=null;
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+                    conn = DriverManager.getConnection(url, "root", "1234");
+
+                    String sql = "INSERT INTO article";
+                    sql += " SET regDate =NOW()";
+                    sql += ", updateDate = NOW()";
+                    sql += ", title =\"" + title + "\"";
+                    sql += ", `body` =\"" + body + "\"";
+
+                    pstmt = conn.prepareStatement(sql);
+                    int affectedRows = pstmt.executeUpdate();
+
+                    System.out.println("affectedRows  : " +  affectedRows);
+                } catch (ClassNotFoundException e) {
+                    System.out.println("드라이버 로딩 실패");
+                } catch (SQLException e) {
+                    System.out.println("에러 :  " + e);
+                } finally {
+                    try {
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (pstmt != null && !pstmt.isClosed()) {
+                            pstmt.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+
                 Article article = new Article(id,title,body);
                 articles.add(article);
-
                 System.out.println("생성된 객체물 :    " + article);
                 System.out.printf("%d번 게시물이 등록 되었습니다.\n", article.id);
             }
